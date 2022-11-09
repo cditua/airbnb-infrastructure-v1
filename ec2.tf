ad356c58d93528b1e30b07b18cc8b071e08603d8
cditua <cditua@gmail.com>
2022-11-08 23:04:41 -0500

    scan fix

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,18 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  ebs_optimized = true 
+
+root_block_device {
+ encrypted     = true
+ }
+
+metadata_options {
+        ...
+       http_endpoint = "enabled"
+       http_tokens   = "required"
+  }
 
   tags = {
     Name = "HelloWorld"
